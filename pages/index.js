f520e5f9e4dded07a6f13554e64c21050890b548
Akinfenwa Gabriel Ayomide <100292401+AyomideWeb@users.noreply.github.com>
2023-03-14 02:24:25 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home';
-export { default as Profile } from './Profile';
-export { default as CreateProject } from './CreateProject';
-export { default as ProjectDetails } from './ProjectDetails';
